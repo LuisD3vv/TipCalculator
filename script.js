6384e0ac1b26798795bfc25c966981a1344d9c04
Lissandrito <luismexico2006@gmail.com>
2025-08-31 20:41:06 -0700

    Improve tip calculation logic and reset button

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 let porcentaje1 = document.getElementById("por1");
 porcentaje1.addEventListener("click", function(){
     let cantidad = document.getElementById("bill").value;
@@ -31,7 +29,7 @@ porcentaje2.addEventListener("click", function(){
     }
     let tipAmount = document.getElementById("tipPerson");
     let tiptotal = document.getElementById("tipTotal");
- // valores locales
+    // valores locales
     let valor = Number(cantidad);
     let numberPerson =  Number(personas);
     let propinaTotal = (valor / numberPerson) * 0.10;
@@ -39,7 +37,7 @@ porcentaje2.addEventListener("click", function(){
 
     tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
     tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
-    
+
 })
 let porcentaje3 = document.getElementById("por3");
 porcentaje3.addEventListener("click", function(){
@@ -106,11 +104,144 @@ porcentaje5.addEventListener("click", function(){
 
 
 let custom = document.getElementById("por6");
-custom.addEventListener("mouseout", function(){
-    if (!custom.value) {
-        alert("Ingresa un valor");
+custom.addEventListener("focusout", function(){
+    let customper = document.getElementById("por6").value;
+    let cantidad = document.getElementById("bill").value;
+    let personas = document.getElementById("nmofpp").value;
+    if (!cantidad && !personas) {
+        alert("Debes de ingresar los datos solicitados");
         return 1;
     }
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+
+    let valor = Number(cantidad);
+    let numberPerson =  Number(personas);
+    let propinaTotal = (valor *( customper / 100))/numberPerson;
+    let totalPersona = (valor / numberPerson) + propinaTotal;
+
+    tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
+    tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
+})
+
+// Reiniciar to-do a la verga
+let botonReinicio = document.getElementById("reset");
+botonReinicio.addEventListener("click", function(){
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+    let custom = document.getElementById("por6").value;
+    custom.innerHTML = "--";
+    tiptotal.textContent = "--";
+    tipAmount.textContent = "--";
+})
+
+l
+let porcentaje1 = document.getElementById("por1");
+porcentaje1.addEventListener("click", function(){
+    let cantidad = document.getElementById("bill").value;
+    let personas = document.getElementById("nmofpp").value;
+    if (!cantidad && !personas) {
+        alert("Debes de ingresar los datos solicitados");
+        return 1;
+    }
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+
+    // valores locales
+    let valor = Number(cantidad);
+    let numberPerson =  Number(personas);
+    let propinaTotal = valor * 0.5;
+    let totalPersona = (valor / numberPerson) + propinaTotal;
+
+    tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
+    tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
+})
+let porcentaje2 = document.getElementById("por2");
+porcentaje2.addEventListener("click", function(){
+    let cantidad = document.getElementById("bill").value;
+    let personas = document.getElementById("nmofpp").value;
+    if (!cantidad && !personas) {
+        alert("Debes de ingresar los datos solicitados");
+        return 1;
+    }
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+    // valores locales
+    let valor = Number(cantidad);
+    let numberPerson =  Number(personas);
+    let propinaTotal = (valor / numberPerson) * 0.10;
+    let totalPersona = (valor / numberPerson) + propinaTotal;
+
+    tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
+    tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
+
+})
+let porcentaje3 = document.getElementById("por3");
+porcentaje3.addEventListener("click", function(){
+    let cantidad = document.getElementById("bill").value;
+    let personas = document.getElementById("nmofpp").value;
+    if (!cantidad && !personas) {
+        alert("Debes de ingresar los datos solicitados");
+        return 1;
+    }
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+
+    // valores locales
+    let valor = Number(cantidad);
+    let numberPerson =  Number(personas);
+    let propinaTotal = (valor / numberPerson) * 0.15;
+    let totalPersona = (valor / numberPerson) + propinaTotal;
+
+    tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
+    tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
+
+
+})
+let porcentaje4 = document.getElementById("por4");
+porcentaje4.addEventListener("click", function(){
+    let cantidad = document.getElementById("bill").value;
+    let personas = document.getElementById("nmofpp").value;
+    if (!cantidad && !personas) {
+        alert("Debes de ingresar los datos solicitados");
+        return 1;
+    }
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+    // valores locales
+    let valor = Number(cantidad);
+    let numberPerson =  Number(personas);
+    let propinaTotal = (valor * 0.25)/numberPerson;
+    let totalPersona = (valor / numberPerson) + propinaTotal;
+
+    tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
+    tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
+
+
+})
+let porcentaje5 = document.getElementById("por5");
+porcentaje5.addEventListener("click", function(){
+    let cantidad = document.getElementById("bill").value;
+    let personas = document.getElementById("nmofpp").value;
+    if (!cantidad && !personas) {
+        alert("Debes de ingresar los datos solicitados");
+        return 1;
+    }
+    let tipAmount = document.getElementById("tipPerson");
+    let tiptotal = document.getElementById("tipTotal");
+
+    let valor = Number(cantidad);
+    let numberPerson =  Number(personas);
+    let propinaTotal = (valor * 0.50)/ numberPerson;
+    let totalPersona = (valor / numberPerson) + propinaTotal;
+
+    tipAmount.innerHTML = "$" + propinaTotal.toFixed(2);
+    tiptotal.innerHTML ="$" + totalPersona.toFixed(2)
+})
+
+
+let custom = document.getElementById("por6");
+custom.addEventListener("focusout", function(){
     let customper = document.getElementById("por6").value;
     let cantidad = document.getElementById("bill").value;
     let personas = document.getElementById("nmofpp").value;
